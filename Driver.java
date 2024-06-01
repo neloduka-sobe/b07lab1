81fb08a51bee8d9bc0fdf7cccd9aef3f833be60c
neloduka-sobe <borys.langowicz@mail.utoronto.ca>
2024-06-01 10:26:30 -0400

    Driver.java

--- Driver.java
+++ Driver.java
@@ -20,7 +20,12 @@ public class Driver {
 
       Polynomial p1_p2 = p1.add(p2);
       p1_p2.printPolynomial();
+      
+      Polynomial p1_p2_p2 = p1_p2.add(p2);
+      p1_p2_p2.printPolynomial();
 
+      Polynomial p1p2 = p1.multiply(p2);
+      p1p2.printPolynomial();
 
       Polynomial p1p3 = p1.multiply(p3);
       p1p3.printPolynomial();
